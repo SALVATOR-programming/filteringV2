2bfe6e9453ca8d2b383390a68cc158933e70331c
SALVATOR <bigratteam@gmail.com>
2024-12-29 02:08:37 -0800

    filtering

---
+++ filteringV2.py
@@ -0,0 +1,75 @@
+import random
+parts = [
+    "£minik",
+    "11.11.C4.12.12",
+    "¥fttks-122334",
+    "darkweb_000_tor",
+    "Virus.ly",
+    "viru3",
+    "gk-999.2.5.4-php",
+    "VIRUSphp",
+    "Null_999999999_salvator_8085_tor_000_Fill_Filtr_Filter",
+    "R0B1KA.bug=/bAg=/Bag=/BuG.=/bUg.FYLLTER",
+    "rest+5.106.8.151,M12,bug-3.5.4-ping,xxx+10.10.34.35",
+    "b0Y0a2cafbaf668e282d2dc02a1fe2a7,Spam%100%2Filter.channel.after.change:x",
+    "x=%d,y=%d,x,y",
+    "bug-3.5.4-badg-000-Bag-999",
+    "M24.gk.C4.ufttk-000,fata",
+    "¥fttk-8085-python.bUg",
+    "ping-error+106.404.151",
+    "Error:-:404:-:rubika.ir",
+    "H.N.Z.7H9",
+    "(/#filtered-фильтроваться-筛选-φιλτραριστεί/)",
+    "/H/A.U.M.R*/(xxx)z/+4.4.3.5.0.4\"fil",
+    "python=/phishing-web-rubika.ir(3.11.6-1)",
+    "bUg</R0Bika>reset_4000_polpy.filtering.filtr.fil.virus.85k.akant.anti-islam.gk",
+    "bug/%12233-4.dev/#2323-24.BaG",
+    "60bdffd3a2be4h",
+    "code-frame/-/code-frame-7.0.0.tgz",
+    "hmmhdNmmmNNMmdssshNdy",
+    "Viru3:-:Error402",
+    "Filter-Filter0_filter000virus000hack000pore/fil.تایم تور.yfttk_01k.in",
+    "noboteubikasupoeraplekshenrubikaserver/www.filtering.rubika.polpy",
+    "reset_22.24.93.220:8000_anti-SupportBot",
+    "Akbaknkknnkq.Filter.hacking.web-rubika",
+    "terminal_تایم تور_termux.hacking-activity.inrubika.0x0.0x0.0x0",
+    "Akbaknkknnkq",
+    "(/Akbaknkknnkq***$Fil/)",
+    "yftt⅕ķ<Filter>ýfțţ⁹⁹k",
+    "Badge:-:Terr.flf/4r4dc9ekcdk.Thviru3.oiu/35.208.137.69,56,84/GHor6.7.hfdjsh.34.36",
+    "teramport,208,91,197,27,9721a1cd79583ddd9ae744fb7b507d5d.30",
+    "noboteubikasupoeraplekshenrubikaserver",
+    "ter.am.fil.ter.af/f7hh82l35k/CT=81.28.49.29/bot_filter.php-fil",
+    "tramport/#sxs.fil{h/g}php=sex-link)362±7120",
+    "reset_99977_polpy/server.offline.online"
+]
+links = [ "https://imgurl.ir/uploads/z9951_InShot_20240224_115729792.jpg", "https://s8.uupload.ir/files/img_20240302_021538_421_5ey.jpg", "https://s8.uupload.ir/files/img_20240302_022230_985_tt52.jpg", "https://imgurl.ir/uploads/i32304_IMG_20240302_125006_833.jpg", "https://imgurl.ir/uploads/a037047_IMG_20240302_125006_953.jpg", "https://imgurl.ir/uploads/g787946_IMG_20240302_125006_103.jpg", "https://s8.uupload.ir/files/images_(3)_4lkb.jpeg", "https://imgurl.ir/uploads/u73287_VID_20240302_170745_435.mp4", "https://imgurl.ir/uploads/j825317_VID_20240302_135748_932.mp4", "https://imgurl.ir/uploads/f477651_VID_20240302_140807_052.mp4", "https://imgurl.ir/uploads/y076030_VID_20240302_140632_131.mp4", "https://imgurl.ir/uploads/q280660_VID_20240302_134920_183.mp4", "https://imgurl.ir/uploads/c18824_4_5962999256506700423.mp4", "https://imgurl.ir/uploads/d15047_af.png", "https://s4.uupload.ir/files/img_20210728_194736_412_9t6h.jpg", "https://imgtr.ee/images/2023/10/17/a07b7180a3a1d43b490fd7a62c8e7d11.png", "https://s8.uupload.ir/files/(2)_zk05.jpg", "https://s8.uupload.ir/files/_video_al6f.jpg", "https://imgurl.ir/uploads/k5289_darkweb.png", "https://s8.uupload.ir/files/img_20230527_223527_960_224m.jpg", "https://s8.uupload.ir/files/crdghzedz1lx_6ew0.jpg", "https://s8.uupload.ir/files/screenshot_%DB%B2%DB%B0%DB%B2%DB%B3%DB%B0%DB%B5%DB%B0%DB%B4-%DB%B1%DB%B6%DB%B2%DB%B6%DB%B5%DB%B5_chrome_yxag.jpg", "https://uploadkon.ir/uploads/c33111_23IMG-20230420-183307-124.jpg", "https://cdn.itsup.com/creatives/41/300442pie4k4.jpg", "https://s8.uupload.ir/files/20230613_130653_xe3r.jpg", "https://uploadkon.ir/uploads/4ddf16_23InShot-%DB%B2%DB%B0%DB%B2%DB%B3%DB%B0%DB%B4%DB%B1%DB%B7-%DB%B0%DB%B2%DB%B4%DB%B7%DB%B2%DB%B2%DB%B3%DB%B4%DB%B8.jpg", "https://uploadkon.ir/uploads/c33111_23IMG-20230420-183307-124.jpg", "https://uploadkon.ir/uploads/08ec03_23InShot-20231101-222651376.jpg", "https://up.20script.ir/file/2f7c-InShot-%DB%B2%DB%B0%DB%B2%DB%B3%DB%B0%DB%B9%DB%B1%DB%B3-%DB%B1%DB%B9%DB%B4%DB%B9%DB%B5%DB%B۸%DB%B۹%DB%B۸%DB%B۷.jpg", "https://imgurl.ir/uploads/o17116_-ografi-XNXX-Rubika-xXx.gif" ]
+bugs = [ "*Filter-Filter0_filter000virus000hack000pore/target=❌/fil.تور.yfttk_01k.in", "https://bit.py3/Virus-link.in.darkWeb.onlain/www.filtering.by", "Fil=/#bug-linux)*fyll.server/anlain/server.of.af)terminal_تور_termux.hacking-activity.inrubika.0x0.0x0.0x0.page.github.com", "https://noboteubikasupoeraplekshenrubikaserver/www.filtering.rubika.polpy=/reset_22.24.93.220:8000_anti-SupportBot/)", "http://fyll.ter.rubika+5.1.0.6.8.1.5.1.h3/Akbaknkknnkq.Filter.hacking.web-rubika.1.0.1.0.3.4.3.5.gk.3.5.2.0.8.1.3.7.6.9/reset_99977_hacked.terminal(12233-4)",'support-rubika.ir / spam account = ❗️/log account = ❗️/ bug-3.5.4-py [<https://docs.python.org/3/bugs.html>] ip logger : 5.106.8.151 [<report.support.py>] ','bug_AI = bug-9.9.9-support {<https://rubika.ir/SupportBot>} ip : 80.249.115.8 tcp {<https://rubika.ir/Netreport>} log account {<report-user.AI>} AI-report : Spam%100','Linux-kali<[Attack=rubika.ir]>log : 80.249.115.8 ; tcp == oflline.server = rubika.ir','Spam.bg/Spam_Account/-999+98/report-account==spam%100|-YftTkS_99977.spm','https://Support.rubika.ir/user.in.rubika.send-sex-photo.and.BuG.yFtt15k.and.send.sexy-video.and.Polpy.Badg.and.send.text-pornografi.xXx',]
+alg = [ "https://fil.fil.fil/02/03/37/0/0/0/02/03/af.of.af/37/0/0/0/02/03/37/Fake.account/0/0/0/02/03/termux.dev/37/0/0/0", "9.9.9.7.7.fil/60bdffd3a2be4h/5.1.0.6.8.1.5.1.gk.3.5.2.0.8.1.3.7.6.9.bug/%12233-4.dev/#2323-24.BaG", "http://8.1.2.8.4.9.2.9.gk/fyll.P+Oad/.BuG?php.85k.io/1111.5y/5.1.0.6.8.1.5.1.1.0.1.0.3.4.3.5(/69.137.208.35.bc.googleusercontent.com", "http://6.2.3.7/k//l/o/4.3.1.0.4.9.9.3.5.6.1/gk.C.h.m.a.f.3.6.2.9.7x.4.6.7.8.ax.b.ai.2.4.5.5.6.3.2.2.4.5.7.9.0.h3/Hs3x/10-10.12//af-//y//h//d//g-/2.3.5.6.7.3.2.9.F.m/zCf4gO5/3.9.1.5.6.2.3.7/12xz0g/2.4.8.1.1.9.5.0.4.4.5.5.4.2.8.6/axhs12zx.Rubika/5.3.5.30", "5.106.8.151.fil/y/gk/1.0.1.0.3.4.3.5.g.8.1.2.8.4.9.2.9/rest_12233.4%polpy", "http://5.1.0.6.8.1.5.1.io/g//bug//h/1.0.1.0.3.4.3.5.6.9.1.3.7.2.0.8.3.5/*filterig-account*/8.1.2.8.4.9.2.9(/reset-122334(/filteri.com", "http://af.of.af/bug-122334%polpy.filterin.ter($Fil.1111-4.ter$)af.5.1.0.6.8.1.5.1*fil-anti-islamic-darkweb-fil*3.5.2.0.8.13.7.6.9.uttk-تایم تور", "http://1.1.1.1/0000/d///f//i///y.6.0.3.2.3.8.6.9.(https://bit.ly/3AeiAuD).5.1*(@SupportBot)*6.8.1.5.1.5.5.1.2.4.0.3.4.X.0.XX.0.x.Fil*0.0.0.0.1.1.1.1.s.Hack.3.4.5.6.6.7.7.7.2.4.0.1.8.4.5.2.7///2.5.0.2.0.Fil", "http://X.0.4.7.13.3/11.4.9.2023.22.32.13.3.1.1.1.2.13.21.11.47.50.000.21.11.47:26.273.3.11.4.127.0.0.1:9050.0.4.7.13.5.4.3///1.0.8.1.1.2.1.4.7.1.4.9.1.7.4.2.0.5.2.1.2.2.2.1.2.5.8.2.8.4.2.9.5.3.0.0.3.0.2.3.0.7.3.0.9.3.3.2.3.3.7f", "https://M.010101.bug/00.01.000.01.10.000.01.00.01.000.10.00.000.10.01/onlline/ReportRubikaSupport/onlline/c-0x0.01.000.00.10.01.00.0x0.01.000.0x0.10.00.01.0x0.00.0x0<(Badge.99977.Server.io)>#Anti=rubika.ir/Netreport\"&\"rubika.ir/SupportBot", "HtTps://cxCxc.bug/+5.106.8.151+/1.5.1.9.7.2.0.4.5.6.1.3.2.4.8.1.6.9.4.8/offline//bug-SUPPORTBOT-server//onlline/3.3.3.1.3.9.3.2.1.0.4.1.4.3.9.1111.3.2.4.8.1.6.9.4.8.15.197.2.0.4.5.6.%CCC-mn.1.0.1.0.3.4.3.5.%1111.bug-01.00.10.000.01.000.00.01.10.000.10.01.00.01.000.10.00.01.0x0.0x0.0x0/-b0Y0a2cafbaf668e282d2dc02a1fe2a7<[@SupportBot]>", "HttpS://BuG.bug/+999/Spam/'Badge-Server=+5.106.8.151+'/report/c-0.0x0.00.01.000.00.01.0x0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.killd-8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.8.88.8085.YfttkS-99.999.99977.999.9.9.9.9.9.9.9.9.9.9.9.99.9.9.9.9.9.9.9.9.9.999.'+999''+98'/-CyberPolice=Report_rubika.ir" ]
+ip = [ "202.210.189.111", "207.244.97.230", "209.41.183.242", "210.118.170.181", "210.240.104.2", "211.115.110.218", "211.18.200.4", "212.109.192.235", "212.109.219.31", "212.26.129.68", "212.47.223.19", "5.106.8.151", "10.10.34.35", "10.10.34.34", "3.33.139.32", "34.206.39.153", "15.197.204.56", "198.74.11.40" ]
+
+print('1-get a mix of profane words\n2-get a sex link\n3-get a bug\n4-get algorithm\n5-get sensitive IP\n-------CODED BY SALVATOR-------')
+choice = input('enter your choice: ')
+
+if choice == '2':
+    ran=random.choice(links)
+    print(f'your link is: {ran}')
+    print('\n\n\n-------CODED BY SALVATOR-------')
+elif choice == '1':
+    random_items = random.sample(parts, 10)
+
+    formatted_items = "/>".join([f"[{item}]" for item in random_items])
+
+    print(formatted_items)
+    print('\n\n\n\n----------CODED BY SALVATOR-------')
+elif choice == '3':
+    ran = random.choice(bugs)
+    print('warning: if you see a emoji in the code,just replace it with your target ID\n\n')
+    print(ran)
+elif choice == '4':
+    ran = random.choice(alg)
+    print('warning: if you see a emoji in the code,just replace it with your target ID\n\n')
+    print(ran)
+elif choice == '5':
+    ran = random.choice(ip)
+    print(f'your sensitive IP is>>>>>> {ran}')
